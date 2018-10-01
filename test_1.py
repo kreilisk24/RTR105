fbeafd0b38e194a3bcf93321a330e28a6b3b06a1
user <karlisk24@gmail.com>
2018-10-01 17:46:41 +0300

    20181001_17_46

---
+++ test_1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+#http://www.cplusplus.com/reference/cstdio/printf/
+
+a = 10
+b = 1.99
+c = 'A'
+print("a = %d"%(a))
+print("b = %.4f"%(b))
+print("c = %s"%(c))
+a = 15452
+b = 9345.1985731
+c = 'A'
+print("a = %d"%(a))
+print("b = %.3f"%(b))
+print("c = %s"%(c))
+
