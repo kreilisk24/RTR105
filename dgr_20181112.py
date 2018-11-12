86a84fa2b80e613774e869013b25a4ada4f0cd7a
user <karlisk24@gmail.com>
2018-11-12 17:58:24 +0200

    20181112_17_58

---
+++ dgr_20181112.py
@@ -0,0 +1,46 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: /home/user/sin_caur_summu_ver3.py =================
+Lietotāj, lūdzu, ievadi argumentu (x): 4.2
+sin(4.20) =  -0.87
+a0 =   4.20 S0 =   4.20
+a1 = -12.35 S1 =  -8.15
+a2 =  10.89 S2 =   2.74
+a3 =  -4.57 S3 =  -1.83
+>>> 
+================= RESTART: /home/user/sin_caur_summu_ver3.py =================
+Lietotāj, lūdzu, ievadi argumentu (x): 1.001
+sin(1.00) =   0.84
+a0 =   1.00 S0 =   1.00
+a1 =  -0.17 S1 =   0.83
+a2 =   0.01 S2 =   0.84
+a3 =  -0.00 S3 =   0.84
+>>> 
+================= RESTART: /home/user/sin_caur_summu_ver4.py =================
+Lietotāj, lūdzu, ievadi argumentu (x): 2.1
+sin(2.10) =   0.86
+a0 =   2.10 S0 =   2.10
+a1 =  -1.54 S1 =   0.56
+a2 =   0.34 S2 =   0.90
+a3 =  -0.04 S3 =   0.86
+>>> 
+================= RESTART: /home/user/sin_caur_summu_ver5.py =================
+Lietotāj, lūdzu, ievadi argumentu (x): 0.333
+sin(0.33) =   0.33
+a0 =   0.33 S0 =   0.33
+a1 =  -0.01 S1 =   0.33
+a2 =   0.00 S2 =   0.33
+a3 =  -0.00 S3 =   0.33
+>>> 
+=============== RESTART: /home/user/sin_caur_summu_galaver.py ===============
+Lietotāj, lūdzu, ievadi argumentu (x): 1.34
+standarta sin(1.34) =   0.97
+Izdruka no liet.f. a0 =   1.34 S0 =   1.34
+Izdruka no liet.f. a1 =  -0.40 S1 =   0.94
+Izdruka no liet.f. a2 =   0.04 S2 =   0.97
+Izdruka no liet.f. a3 =  -0.00 S3 =   0.97
+Izdruka no liet.f. Beigas!
+mans sin(1.34) =   0.97
+>>> 
