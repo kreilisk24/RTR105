c97f02e7267b08555e5378565e9d49da7068f541
Karlis <karlisk24@gmail.com>
2018-11-21 17:49:09 +0200

    20181121_17_49

---
+++ dgr_20181121.py
@@ -0,0 +1,53 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=================== RESTART: /home/kreilisk/RTR105/cos.py ===================
+Cos(x^2) aprekinasana:
+Ievadiet argumentu x: 2.27
+cos(2.27*2.27) =   0.43
+a0 =   1.00 S0 =   1.00
+a499 =  -0.00 S499 =                                                       0.426402
+a500 =   0.00 S500 =                                                       0.426402
+cos(x^2) caur summu:   0.43
+>>> 
+=================== RESTART: /home/kreilisk/RTR105/cos.py ===================
+Cos(x^2) aprekinasana:
+Ievadiet argumentu x: 2.27
+cos(2.27*2.27) =   0.43
+a0 =   1.00 S0 =   1.00
+a499 =  -0.00 S499 =                                                              0
+a500 =   0.00 S500 =                                                              0
+cos(x^2) caur summu:   0.43
+>>> 
+=================== RESTART: /home/kreilisk/RTR105/cos.py ===================
+Cos(x^2) aprekinasana:
+Ievadiet argumentu x: 2.27
+cos(2.27*2.27) =   0.43
+a0 =   1.00 S0 =   1.00
+a499 =  -0.00 S499 =   0.43
+a500 =   0.00 S500 =   0.43
+cos(x^2) caur summu:   0.43
+>>> 
+=================== RESTART: /home/kreilisk/RTR105/cos.py ===================
+Cos(x^2) aprekinasana:
+Ievadiet argumentu x: 2.27
+cos(2.27*2.27) =   0.43
+a0 =   1.00 S0 =   1.00
+a499 =  -0.00 S499 =   0.43
+a500 =   0.00 S500 =   0.43
+cos(x^2) caur summu:   0.43
+                500
+               _____
+               \            k     4*k
+                \       (-1)  *  x
+cos(2.27*2.27) = >    _________________
+                /
+               /____       (2 * k)!
+                k=0
+                                    4
+                            (-1) * x
+rekurences reizinatajs: ______________________
+
+                         (2 * k) * (2 * k - 1)
+>>> 
