7373d37ec15ee5587886e20ca129e4a57b8cca3e
Karlis <karlisk24@gmail.com>
2018-12-04 22:51:03 +0200

    20181204_22_50

--- 171.py
+++ 171.py
@@ -1,4 +1,4 @@
-# Fails: 170.py
+# Fails: 171.py
 # Autors: Karlis Kreilis
 # Apliecibas numurs: 260REB181
 # Datums: 03.12.2018
@@ -8,25 +8,41 @@
 from math import cos, fabs
 from time import sleep
 
-def f(x):
-    return cos(x**2)
+def cos_arg_kvadrataa(x):
+    k = 0
+    while k < 501:
+        if k == 0:
+            a = ((-1)**0*float(x)**0)/(1)
+            S = float(a)
+        k = k + 1
+        R = ((-1)**1*float(x)**4)/((2*k)*(2*k-1))
+        a = a * float(R)
+        S = S + a
+    return S
 
-a = 1
-b = 12
+a = 1.1
+b = 1.7
 
 # Aprekjinam funkcijas vertibas dotajos punktos:
-funa = f(a)
-funb = f(b)
-
+funa = cos_arg_kvadrataa(a)
+funb = cos_arg_kvadrataa(b)
+if ( funa * funb > 0.0 ):
+    print("Dotajaa intervaalaa saknu nav", a, b)
+else:
+    print("Dotajaa intervaalaa sakne(s) ir!")
 # Precizitate
 deltax = 0.0001
 k = 0
 # Sashaurinaashana
-while ( fabs(b-a) > deltax):
-    x = (a+b)/2; funx = f(x)
+while ( fabs(float(b)-float(a)) > deltax):
     k = k + 1
+    x = (float(a)+float(b))/2
+    funx = cos_arg_kvadrataa(x)
     if ( funa*funx < 0. ):
         b = x
     else:
         a = x
-print "Saknes ir: ", x
+print("Funkcijas saknes vertiba: x = %.4f "%x)
+print("Dotas funkcijas vertiba saja punkta: cos(x^2) = %f "%cos_arg_kvadrataa(x))
+print("Nepieciesamo iteraciju skaits intervalu dalisanai uz pusem: k = ",k)
+
---
+++ dgr_20181204.py
@@ -0,0 +1,126 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+==== No Subprocess ====
+
+WARNING: Running IDLE without a Subprocess is deprecated
+and will be removed in a later version. See Help/IDLE Help
+for details.
+
+>>> 
+Ievadiet intervala sakumpunktu:2.6
+Ievadiet intervala beigu punktu:3.6
+Dotajaa intervaalaa saknu nav 2.6 3.6
+Traceback (most recent call last):
+  File "/usr/lib/python3.6/idlelib/pyshell.py", line 767, in runcode
+    exec(code, self.locals)
+  File "/home/kreilisk/RTR105/171.py", line 31, in <module>
+    sleep(1); exit()
+  File "/usr/lib/python3.6/_sitebuiltins.py", line 26, in __call__
+    raise SystemExit(code)
+SystemExit: None
+>>> 
+Ievadiet intervala sakumpunktu:2.6
+Ievadiet intervala beigu punktu:3.6
+Dotajaa intervaalaa saknu nav 2.6 3.6
+0.8884613290130879 0.9235231208616212
+Traceback (most recent call last):
+  File "/usr/lib/python3.6/idlelib/pyshell.py", line 767, in runcode
+    exec(code, self.locals)
+  File "/home/kreilisk/RTR105/171.py", line 41, in <module>
+    funx = f(x)
+NameError: name 'f' is not defined
+>>> 
+Ievadiet intervala sakumpunktu:1.1
+Ievadiet intervala beigu punktu:1.6
+Dotajaa intervaalaa sakne(s) ir!
+0.3530194012193302 -0.8355887771314076
+Traceback (most recent call last):
+  File "/usr/lib/python3.6/idlelib/pyshell.py", line 767, in runcode
+    exec(code, self.locals)
+  File "/home/kreilisk/RTR105/171.py", line 41, in <module>
+    funx = f(x)
+NameError: name 'f' is not defined
+>>> 
+Ievadiet intervala sakumpunktu:1.1
+Ievadiet intervala beigu punktu:1.7
+Dotajaa intervaalaa sakne(s) ir!
+0.3530194012193302 -0.9685171642284461
+Funkcijas saknes vertiba: x =  1.2532958984375
+Traceback (most recent call last):
+  File "/usr/lib/python3.6/idlelib/pyshell.py", line 767, in runcode
+    exec(code, self.locals)
+  File "/home/kreilisk/RTR105/171.py", line 47, in <module>
+    print("Dotas funkcijas vertiba saja punkta: cos(x^2) = ",S)
+NameError: name 'S' is not defined
+>>> 
+Traceback (most recent call last):
+  File "/usr/lib/python3.6/idlelib/pyshell.py", line 767, in runcode
+    exec(code, self.locals)
+  File "/home/kreilisk/RTR105/171.py", line 22, in <module>
+    l = S
+NameError: name 'S' is not defined
+>>> 
+Traceback (most recent call last):
+  File "/usr/lib/python3.6/idlelib/pyshell.py", line 767, in runcode
+    exec(code, self.locals)
+  File "/home/kreilisk/RTR105/171.py", line 22, in <module>
+    print(S)
+NameError: name 'S' is not defined
+>>> 
+Ievadiet intervala sakumpunktu:1.1
+Ievadiet intervala beigu punktu:1.6
+0.3530194012193302
+-0.8355887771314076
+Traceback (most recent call last):
+  File "/usr/lib/python3.6/idlelib/pyshell.py", line 767, in runcode
+    exec(code, self.locals)
+  File "/home/kreilisk/RTR105/171.py", line 29, in <module>
+    if ( funa * funb > 0.0 ):
+TypeError: unsupported operand type(s) for *: 'NoneType' and 'NoneType'
+>>> 
+Ievadiet intervala sakumpunktu:1.1
+Ievadiet intervala beigu punktu:1.6
+Dotajaa intervaalaa sakne(s) ir!
+0.3530194012193302 -0.8355887771314076
+Funkcijas saknes vertiba: x =  1.25325927734375
+Dotas funkcijas vertiba saja punkta: cos(x^2) =  0.0001375105462845044
+Nepieciesamo iteraciju skaits intervalu dalisanai uz pusem: k =  13
+>>> 
+Ievadiet intervala sakumpunktu:1.1
+Ievadiet intervala beigu punktu:
+Traceback (most recent call last):
+  File "/usr/lib/python3.6/idlelib/pyshell.py", line 767, in runcode
+    exec(code, self.locals)
+  File "/home/kreilisk/RTR105/171.py", line 28, in <module>
+    funb = cos_arg_kvadrataa(b)
+  File "/home/kreilisk/RTR105/171.py", line 15, in cos_arg_kvadrataa
+    a = ((-1)**0*float(x)**0)/(1)
+ValueError: could not convert string to float: 
+>>> 
+Ievadiet intervala sakumpunktu:1.1
+Ievadiet intervala beigu punktu:1.7
+Dotajaa intervaalaa sakne(s) ir!
+0.3530194012193302 -0.9685171642284461
+Funkcijas saknes vertiba: x = 1.2533 
+Dotas funkcijas vertiba saja punkta: cos(x^2) = 0.0000 
+Nepieciesamo iteraciju skaits intervalu dalisanai uz pusem: k =  13
+>>> 
+Ievadiet intervala sakumpunktu:1.1
+Ievadiet intervala beigu punktu:1.8
+Dotajaa intervaalaa sakne(s) ir!
+0.3530194012193302 -0.9951619033238306
+Funkcijas saknes vertiba: x = 1.2534 
+Dotas funkcijas vertiba saja punkta: cos(x^2) = -0.00017 
+Nepieciesamo iteraciju skaits intervalu dalisanai uz pusem: k =  13
+>>> 
+Dotajaa intervaalaa sakne(s) ir!
+Funkcijas saknes vertiba: x = 1.2533 
+Dotas funkcijas vertiba saja punkta: cos(x^2) = 0.00005 
+Nepieciesamo iteraciju skaits intervalu dalisanai uz pusem: k =  13
+>>> 
+Dotajaa intervaalaa sakne(s) ir!
+Funkcijas saknes vertiba: x = 1.2533 
+Dotas funkcijas vertiba saja punkta: cos(x^2) = 0.000046 
+Nepieciesamo iteraciju skaits intervalu dalisanai uz pusem: k =  13
+>>> 
