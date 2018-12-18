3af6efc15422866610544277ae8f8b14b51daec4
Karlis <karlisk24@gmail.com>
2018-12-18 14:54:51 +0200

    20181218_14_54

--- 181.py
+++ 181.py
@@ -4,22 +4,12 @@ sys.path.append('/usr/local/anaconda3/lib/python3.6/site-packages')
 # N vienmieriigi sadaliiti gadiijuma skaitlji
 # N uniformly distributed random numbers
 import numpy
+from math import cos
 from numpy import random
 #print(random.__doc__)
 #print(random.uniform.__doc__)
-def f(x):
-    k = 0
-    while k < 501:
-        if k == 0:
-            a = ((-1)**0*float(x)**0)/(1)
-            S = float(a)
-        k = k + 1
-        R = ((-1)**1*float(x)**4)/((2*k)*(2*k-1))
-        a = a * float(R)
-        S = S + a
-    return S
-N = 10000
-l = -2
+N = 2500
+l = -1
 c = 4
 #pseido-gadiijuma skaitlju generatora grauds
 #random.seed(1)
@@ -50,10 +40,10 @@ plt.title('Funkcija un taas integraalis (laukums starp funkciju un x ass)')
 #plt.plot(x,y,'ko')
 N1 = 0
 for i in range(N):
-    if y[i] > 0 and y[i] < f(x[i]) :
+    if y[i] > 0 and y[i] < cos(x[i]**2) :
         plt.plot(x[i],y[i],'go')
         N1 = N1 + 1
-    elif y[i] < 0 and y[i] > f(x[i]):
+    elif y[i] < 0 and y[i] > cos(x[i]**2):
         plt.plot(x[i],y[i],'go')
         N1 = N1 - 1
     else:
