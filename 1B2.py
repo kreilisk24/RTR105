1e326eef8889898c10a8d76800aafe0f8cad00c8
Karlis <karlisk24@gmail.com>
2018-12-28 15:21:48 +0200

    20181228_15_00

--- 1B2.py
+++ 1B2.py
@@ -2,13 +2,15 @@ import sys
 sys.path.append('/usr/local/anaconda3/lib/python3.6/site-packages')
 from numpy import *
 x = linspace(0, 4, 70)
+#print(x)
 y = sin(x)
+#print(y)
 f1 = x
-f = 1*2*3
-s = (x**3/float(f))
+f = float(1*2*3)
+s = (x**3/f)
 f2 = f1 - s
-f3 = f2 + (x**5)/(float(f)*4*5)
-f4 = f3 - (x**7)/(float(f)*4*5*6*7)
+f3 = f2 + (x**5)/(f*4*5)
+f4 = f3 - (x**7)/(f*4*5*6*7)
 from matplotlib import pyplot as plt
 plt.grid()
 plt.xlabel("x")
